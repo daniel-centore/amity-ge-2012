9ed046ea13b7a2bb70d553685395629ef268ef24
dcentore <dcentore@DrD-Wee>
2012-02-19 15:44:37 -0500

    formatted code pretty

---
+++ GE2012/src/ge2012/GE2012.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ge2012;
+
+/**
+ *
+ * @author dcentore
+ */
+public class GE2012
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello folks");
+    }
+}
