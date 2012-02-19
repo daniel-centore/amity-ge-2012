08ac19ef6f793024e100a61f76354257fda39704
drdanielfc <drdanielfc@localhost>
2012-02-19 15:35:07 -0500

    configured the project for eclipse

--- GE2012/src/ge2012/GE2012.java
+++ GE2012/src/ge2012/GE2012.java
@@ -14,6 +14,6 @@ public class GE2012 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello folks");
     }
 }
