b87a25cb65f45b7f818aeb25c13a6b135702e629
drdanielfc <drdanielfc@localhost>
2012-02-20 14:55:30 -0500

    (That was weird) This is getting put on my local hard drive :-)

--- GE2012/src/ge2012/GE2012.java
+++ GE2012/src/ge2012/GE2012.java
@@ -17,5 +17,7 @@ public class GE2012
     public static void main(String[] args)
     {
         System.out.println("Hello folks");
+        System.out.println("Okay, so what I'm going to show you how to do is commit.");
+        System.out.println("Committing in mercurial (hg) is NOT like svn. It only keeps it on your local hard drive. Watch.");
     }
 }
