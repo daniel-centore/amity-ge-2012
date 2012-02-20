b54f929a9fc37f45f92f41e3b091626c87fcb3ea
drdanielfc <drdanielfc@localhost>
2012-02-20 14:58:10 -0500

    getting readt......

--- GE2012/src/ge2012/GE2012.java
+++ GE2012/src/ge2012/GE2012.java
@@ -19,5 +19,16 @@ public class GE2012
         System.out.println("Hello folks");
         System.out.println("Okay, so what I'm going to show you how to do is commit.");
         System.out.println("Committing in mercurial (hg) is NOT like svn. It only keeps it on your local hard drive. Watch.");
+        
+        System.out.println("It went away! Okay, we just reverted a code change. Now let's make a change that I decide is ready for the main repo.");
+        
+        foo();
+        
+        System.out.println("Okay, first lets commit this to mine.");
+    }
+    
+    static void foo()
+    {
+    	
     }
 }
