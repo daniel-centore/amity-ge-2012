47d4eba93e98f4205e369b144b7b48946d13d2c2
drdanielfc <drdanielfc@localhost>
2012-03-02 22:20:11 -0500

    basically finished the gui

--- GE2012/src/game/Tournament.java
+++ GE2012/src/game/Tournament.java
@@ -1,4 +1,5 @@
 package game;
+
 import hex.HexMove;
 import hex.HexState;
 
@@ -7,16 +8,19 @@ import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Tournament {
+public class Tournament
+{
 	public static Params tournamentParams;
 	public static Params gameParams;
 
-	public static Client [] clients;
+	public static Client[] clients;
 	public static ArrayList<GameThread> threads;
 
-	private static class Game {
+	private static class Game
+	{
 		public int h, a;
 		public int gameNum;
+
 		public Game(int h, int a, int n)
 		{
 			this.h = h;
@@ -32,17 +36,19 @@ public class Tournament {
 		else
 			return name.substring(0, 13);
 	}
+
 	private static int randInt(int lo, int hi)
 	{
 		int delta = hi - lo + 1;
-		return (int)(Math.random() * delta) + lo;
+		return (int) (Math.random() * delta) + lo;
 	}
 
 	private static void shuffle(ArrayList<Game> list)
 	{
 		int sz = list.size();
-		for (int i=0; i<sz; i++) {
-			int newSpot = randInt(i, sz-1);
+		for (int i = 0; i < sz; i++)
+		{
+			int newSpot = randInt(i, sz - 1);
 
 			Game old = list.get(i);
 			Game neww = list.get(newSpot);
@@ -50,12 +56,22 @@ public class Tournament {
 			list.set(newSpot, old);
 		}
 	}
+
 	public static boolean randomTeam(int t)
-	{ return !clients[t].deterministic; }
+	{
+		return !clients[t].deterministic;
+	}
+
 	public static boolean stochasticGame(int h, int a)
-	{ return randomTeam(h) || randomTeam(a); }
+	{
+		return randomTeam(h) || randomTeam(a);
+	}
+
 	public static boolean deterministicGame(int h, int a)
-	{ return !stochasticGame(h, a); }
+	{
+		return !stochasticGame(h, a);
+	}
+
 	public static void launchTournament(GameState st, GameMove move)
 	{
 		File dumpsDir = new File("Dumps");
@@ -78,38 +94,48 @@ public class Tournament {
 
 		int p1, p2;
 		int i;
-		int headToHeadSummary[][][] = new int [NUM_CLIENTS][NUM_CLIENTS][3];
-		int homeSummary[][] = new int [NUM_CLIENTS][3];
-		int awaySummary[][] = new int [NUM_CLIENTS][3];
-		int totalSummary[][] = new int [NUM_CLIENTS][3];
+		int headToHeadSummary[][][] = new int[NUM_CLIENTS][NUM_CLIENTS][3];
+		int homeSummary[][] = new int[NUM_CLIENTS][3];
+		int awaySummary[][] = new int[NUM_CLIENTS][3];
+		int totalSummary[][] = new int[NUM_CLIENTS][3];
 		ArrayList<Game> schedule = new ArrayList<Game>();
 		threads = new ArrayList<GameThread>();
 
-		try {
+		try
+		{
 			ServerSocket socket = new ServerSocket(PORT);
-			clients = new Client [NUM_CLIENTS];
-			for (i=0; i<NUM_CLIENTS; i++) {
+			clients = new Client[NUM_CLIENTS];
+			for (i = 0; i < NUM_CLIENTS; i++)
+			{
 				clients[i] = new Client(socket, INIT_LIMIT, MOVE_LIMIT, FINAL_PLAY_LIMIT,
 						GAME_LIMIT, MAX_WARNINGS);
-				if (clients[i].name.toUpperCase().contains("HUMAN")) {
-					clients[i].moveLimit = clients[i].finalPlayLimit = gameParams.integer("HUMANTIME");  
-					clients[i].gameTimeLimit = 10000;  
+				if (clients[i].name.toUpperCase().contains("HUMAN"))
+				{
+					clients[i].moveLimit = clients[i].finalPlayLimit = gameParams.integer("HUMANTIME");
+					clients[i].gameTimeLimit = 10000;
 				}
 				File subdir = new File(dumpDir + "/" + clients[i].name);
 				subdir.mkdir();
 				System.out.printf("%s has joined%n", clients[i].name);
 			}
 
-			for (p1=0; p1<NUM_CLIENTS; p1++) {
-				for (p2=0; p2<NUM_CLIENTS; p2++) {
-					if (p1 == p2) continue;
+			for (p1 = 0; p1 < NUM_CLIENTS; p1++)
+			{
+				for (p2 = 0; p2 < NUM_CLIENTS; p2++)
+				{
+					if (p1 == p2)
+						continue;
 					int numToPlay;
-					if (stochasticGame(p1, p2)) {
+					if (stochasticGame(p1, p2))
+					{
 						numToPlay = NUM_GAMES;
-					} else {
+					}
+					else
+					{
 						numToPlay = 1;
 					}
-					for (i=0; i<numToPlay; i++) {
+					for (i = 0; i < numToPlay; i++)
+					{
 						schedule.add(new Game(p1, p2, i));
 					}
 				}
@@ -118,53 +144,66 @@ public class Tournament {
 
 			int remain = schedule.size();
 
-			while (remain > 0) {
+			while (remain > 0)
+			{
 				Thread.sleep(500);
 				boolean cont = true;
-				while (cont) {
+				while (cont)
+				{
 					cont = false;
-					for (i=0; i<schedule.size(); i++) {
+					for (i = 0; i < schedule.size(); i++)
+					{
 						Game g = schedule.get(i);
 						p1 = g.h;
 						p2 = g.a;
 						int consecWins = Tournament.tournamentParams.integer("CONSECWINS");
 						if ((headToHeadSummary[p1][p2][0] >= consecWins &&
 								headToHeadSummary[p1][p2][1] + headToHeadSummary[p1][p2][2] == 0) ||
-								(headToHeadSummary[p1][p2][0] + headToHeadSummary[p1][p2][2] == 0 && 
-								headToHeadSummary[p1][p2][1] >= consecWins)) {
+								(headToHeadSummary[p1][p2][0] + headToHeadSummary[p1][p2][2] == 0 &&
+								headToHeadSummary[p1][p2][1] >= consecWins))
+						{
 							schedule.remove(i);
 							remain--;
-							if (headToHeadSummary[p1][p2][0] == 0) {
-								headToHeadSummary[p1][p2][1]++; 
+							if (headToHeadSummary[p1][p2][0] == 0)
+							{
+								headToHeadSummary[p1][p2][1]++;
 								homeSummary[p1][1]++;
 								awaySummary[p2][0]++;
 								totalSummary[p1][1]++;
 								totalSummary[p2][0]++;
-							} else {
-								headToHeadSummary[p1][p2][0]++; 
+							}
+							else
+							{
+								headToHeadSummary[p1][p2][0]++;
 								homeSummary[p1][0]++;
 								awaySummary[p2][1]++;
 								totalSummary[p1][0]++;
 								totalSummary[p2][1]++;
 							}
-						} else if (!clients[g.h].busy && !clients[g.a].busy) {
+						}
+						else if (!clients[g.h].busy && !clients[g.a].busy)
+						{
 							GameThread game = new GameThread(clients[g.h], g.h, clients[g.a], g.a,
 									g.gameNum, move.clone(), st.clone());
 							game.start();
 							clients[g.h].busy = clients[g.a].busy = true;
 							threads.add(game);
 							schedule.remove(i);
-							System.out.printf("%s-%s (%d)%n", clients[g.h].name, clients[g.a].name, remain);
+							// System.out.printf("%s-%s (%d)%n", clients[g.h].name, clients[g.a].name, remain);
 							cont = true;
 						}
 					}
 				}
 				i = 0;
-				while (i<threads.size()) {
+				while (i < threads.size())
+				{
 					GameThread t = threads.get(i);
-					if (t.isAlive()) {
+					if (t.isAlive())
+					{
 						i++;
-					} else {
+					}
+					else
+					{
 						p1 = t.homeID;
 						p2 = t.awayID;
 						clients[p1].busy = clients[p2].busy = false;
@@ -173,97 +212,113 @@ public class Tournament {
 						remain--;
 
 						int cnt = stochasticGame(p1, p2) ? 1 : NUM_GAMES;
-						if (outcome == GameState.Status.HOME_WIN) {
+						if (outcome == GameState.Status.HOME_WIN)
+						{
 							System.out.printf("home (%s) won%n", clients[p1].name);
 							headToHeadSummary[p1][p2][0] += cnt;
 							awaySummary[p2][1] += cnt;
 							homeSummary[p1][0] += cnt;
 							totalSummary[p1][0] += cnt;
 							totalSummary[p2][1] += cnt;
-						} else if (outcome == GameState.Status.AWAY_WIN) {
+						}
+						else if (outcome == GameState.Status.AWAY_WIN)
+						{
 							System.out.printf("away (%s) won%n", clients[p2].name);
 							headToHeadSummary[p1][p2][1] += cnt;
 							awaySummary[p2][0] += cnt;
 							homeSummary[p1][1] += cnt;
 							totalSummary[p1][1] += cnt;
 							totalSummary[p2][0] += cnt;
-						} else if (outcome == GameState.Status.DRAW) {
+						}
+						else if (outcome == GameState.Status.DRAW)
+						{
 							System.out.println(" draw\n");
 							headToHeadSummary[p1][p2][2] += cnt;
 							awaySummary[p2][2] += cnt;
 							homeSummary[p1][2] += cnt;
 							totalSummary[p1][2] += cnt;
 							totalSummary[p2][2] += cnt;
-						} else {
+						}
+						else
+						{
 							System.err.println("Error with game outcome");
 						}
 					}
 				}
 			}
 
-			for (int j=0; j<NUM_CLIENTS; j++) {
+			for (int j = 0; j < NUM_CLIENTS; j++)
+			{
 				clients[j].simpleMsg("DONE");
 			}
-			System.out.printf("%13s ", "");
-			for (p1=0; p1<NUM_CLIENTS; p1++) {
-				System.out.printf("%13s ", printableName(clients[p1].name));
+			// System.out.printf("%13s ", "");
+			for (p1 = 0; p1 < NUM_CLIENTS; p1++)
+			{
+				// System.out.printf("%13s ", printableName(clients[p1].name));
 			}
 			System.out.println();
 
-			for (p1=0; p1<NUM_CLIENTS; p1++) {
-				System.out.printf("%13s ", printableName(clients[p1].name));
-				for (p2=0; p2<NUM_CLIENTS; p2++) {
-					if (p1 == p2) {
-						System.out.printf("%13s ", "");
-					} else {
-						System.out.printf("(%3d %3d %3d) ", headToHeadSummary[p1][p2][0],
-								headToHeadSummary[p1][p2][1],
-								headToHeadSummary[p1][p2][2]);
+			for (p1 = 0; p1 < NUM_CLIENTS; p1++)
+			{
+				// System.out.printf("%13s ", printableName(clients[p1].name));
+				for (p2 = 0; p2 < NUM_CLIENTS; p2++)
+				{
+					if (p1 == p2)
+					{
+						// System.out.printf("%13s ", "");
+					}
+					else
+					{
+						// System.out.printf("(%3d %3d %3d) ", headToHeadSummary[p1][p2][0],
+						// headToHeadSummary[p1][p2][1],
+						// headToHeadSummary[p1][p2][2]);
 					}
 				}
-				System.out.printf("(%3d %3d %3d)%n", homeSummary[p1][0],
-						homeSummary[p1][1],
-						homeSummary[p1][2]);
+				// System.out.printf("(%3d %3d %3d)%n", homeSummary[p1][0],
+				// homeSummary[p1][1],
+				// homeSummary[p1][2]);
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e)
+		{
 			System.out.println("Server problem" + e);
 		}
 
-		System.out.printf("%13s ", "");
-		for (p1=0; p1<NUM_CLIENTS; p1++) {
-			System.out.printf("(%3d %3d %3d) ", awaySummary[p1][0],
-					awaySummary[p1][1],
-					awaySummary[p1][2]);
+		// System.out.printf("%13s ", "");
+		for (p1 = 0; p1 < NUM_CLIENTS; p1++)
+		{
+			// System.out.printf("(%3d %3d %3d) ", awaySummary[p1][0],
+			// awaySummary[p1][1],
+			// awaySummary[p1][2]);
 		}
 
-		System.out.printf("%n%n");
-		for (p1=0; p1<NUM_CLIENTS; p1++) {
-			System.out.printf("%13s %3s (%3d %3d %3d)%n", printableName(clients[p1].name),
-					clients[p1].DQd ? "DQd" : "  ",
-							totalSummary[p1][0], totalSummary[p1][1],
-							totalSummary[p1][2]);
+		// System.out.printf("%n%n");
+		for (p1 = 0; p1 < NUM_CLIENTS; p1++)
+		{
+			// System.out.printf("%13s %3s (%3d %3d %3d)%n", printableName(clients[p1].name),
+			// clients[p1].DQd ? "DQd" : "  ",
+			// totalSummary[p1][0], totalSummary[p1][1],
+			// totalSummary[p1][2]);
 		}
-		System.out.println();
+		// System.out.println();
 	}
 
-//	public static void main(String[] args)
-//	{
-//		tournamentParams = new Params("config/tournament.txt");
-//		System.out.println("Starting Hex tournament");
-//		System.out.printf("%d clients%n", tournamentParams.integer("NUMCLIENTS"));
-//		gameParams = new Params("config/hex.txt");
-//		Tournament.launchTournament(new HexState(),	new HexMove());
-//		System.out.println("Tournament is over");
-//	}
-	
+	// public static void main(String[] args)
+	// {
+	// tournamentParams = new Params("config/tournament.txt");
+	// System.out.println("Starting Hex tournament");
+	// System.out.printf("%d clients%n", tournamentParams.integer("NUMCLIENTS"));
+	// gameParams = new Params("config/hex.txt");
+	// Tournament.launchTournament(new HexState(), new HexMove());
+	// System.out.println("Tournament is over");
+	// }
+
 	public static void initializeServer()
 	{
 		tournamentParams = new Params("config/tournament.txt");
 		System.out.println("Starting Hex tournament");
-//		System.out.printf("%d clients%n", tournamentParams.integer("NUMCLIENTS"));
+		// System.out.printf("%d clients%n", tournamentParams.integer("NUMCLIENTS"));
 		gameParams = new Params("config/hex.txt");
-		Tournament.launchTournament(new HexState(),	new HexMove());
+		Tournament.launchTournament(new HexState(), new HexMove());
 		System.out.println("Tournament is over");
 	}
 }
