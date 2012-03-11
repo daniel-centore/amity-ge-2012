82303b676ed8200dc02cf19456025be47f93bd3e
drdanielfc <drdanielfc@localhost>
2012-03-11 15:06:17 -0400

    started work on commit panel

---
+++ GE2012/src/hex/HumanHexPlayer.java
@@ -0,0 +1,96 @@
+package hex;
+
+import game.*;
+import javax.swing.*;
+
+import solution.debug.DebugWindow;
+
+public class HumanHexPlayer extends RandomHexPlayer
+{
+	private GameFrame frame;
+	private HexMove move = new HexMove(0, 0);
+
+	public String messageForOpponent(String opponent)
+	{
+		return "I'm a humanist";
+	}
+
+	public HumanHexPlayer(String nname)
+	{
+		super(nname);
+		frame = new GameFrame(nickname, new HexCanvas());
+		frame.setVisible(true);
+		frame.setResizable(false);
+		gameState = new HexState();
+	}
+
+	public void timeOfLastMove(double secs)
+	{
+
+		// System.out.println("" + secs);
+	}
+
+	public GameMove getMove(GameState game, String lastMove)
+	{
+		DebugWindow.println("Hello");
+
+		char ch = side == GameState.Who.HOME ? HexState.homeSym : HexState.awaySym;
+		frame.setTitle("My move (" + ch + ")");
+		if (!lastMove.equals("--") && frame.canvas.move != null)
+		{
+			((HexMove) frame.canvas.move).parseMove(lastMove);
+		}
+
+		boolean OK;
+		do
+		{
+			frame.canvas.setBoard(game);
+			frame.canvas.repaint();
+			frame.canvas.getMove(move, game, this);
+
+			try
+			{
+				frame.canvas.ready.acquire();
+			} catch (Exception e)
+			{
+			}
+			OK = game.moveOK(move);
+			if (!OK)
+			{
+				if ((move.col == 5 || move.row == 5) && game.getNumMoves() <= 1)
+					JOptionPane.showMessageDialog(null, "You may not start in the middle!", "Illegal Move", JOptionPane.ERROR_MESSAGE);
+
+				// otherwise you clicked on the line which we don't care about
+			}
+		} while (!OK);
+
+		game.makeMove(move);
+		System.out.println(game);
+		frame.canvas.repaint();
+		frame.setTitle("Waiting");
+		return move;
+	}
+
+	public void endGame(int result)
+	{
+		char ch = side == GameState.Who.HOME ? HexState.homeSym : HexState.awaySym;
+		if (result == 1)
+		{
+			frame.setTitle("Won (" + ch + ")");
+		}
+		else if (result == -1)
+		{
+			frame.setTitle("Loss (" + ch + ")");
+		}
+		else
+		{
+			frame.setTitle("Draw (" + ch + ")");
+		}
+	}
+
+	// public static void main(String[] args)
+	// {
+	// GamePlayer p = new HumanHexPlayer("HUMAN");
+	// p.compete(args);
+	// }
+}
---
+++ GE2012/src/solution/HexPlayer_Amity.java
@@ -0,0 +1,93 @@
+package solution;
+
+import java.util.ArrayList;
+
+import hex.*;
+import game.*;
+
+public class HexPlayer_Amity extends GamePlayer
+{
+
+	public HexPlayer_Amity()
+	{
+		super("Amity Regional High School", new HexState(), false);
+	}
+
+	/**
+	 * Initializes the player at the beginning of the tournament. This is called
+	 * once, before any games are played. This function must return within
+	 * the time alloted by the game timing parameters. Default behavior is
+	 * to do nothing.
+	 */
+	public void init()
+	{
+	}
+
+	/**
+	 * This is called at the start of a new game. This should be relatively
+	 * fast, as the player must be ready to respond to a move request, which
+	 * should come shortly thereafter. The side being played (HOME or AWAY)
+	 * is stored in the side data member. Default behavior is to do nothing. 
+	 * @param opponent Name of the opponent being played
+	 */
+	public void startGame(String opponent)
+	{
+	}
+
+	/**
+	 * Called to inform the player how long the last move took. This can
+	 * be used to calibrate the player's search depth. Default behavior is
+	 * to do nothing.
+	 * @param secs Time for the server to receive the last move
+	 */
+	public void timeOfLastMove(double secs)
+	{
+	}
+
+	/**
+	 * Called when the game has ended. Default behavior is to do nothing. 
+	 * @param result -1 if loss, 0 if draw, +1 if win
+	 */
+	public void endGame(int result)
+	{
+	}
+
+	/**
+	 * Called at the end of the tournament. Can be used to do
+	 * housekeeping tasks. Default behavior is to do nothing.
+	 */
+	public void done()
+	{
+	}
+
+	/**
+	 * Produces the player's move, given the current state of the game.
+	 * This function must return a value within the time alloted by the
+	 * game timing parameters.
+	 * 
+	 * @param state Current state of the game
+	 * @param lastMv Opponent's last move. "--" if it is game's first move. 
+	 * @return Player's move
+	 */
+	public GameMove getMove(GameState state, String lastMove)
+	{
+		HexState board = (HexState) state;
+		ArrayList<HexMove> list = new ArrayList<HexMove>();
+		HexMove mv = new HexMove();
+		for (int r = 0; r < HexState.N; r++)
+		{
+			for (int c = 0; c < HexState.N; c++)
+			{
+				mv.row = r;
+				mv.col = c;
+				if (board.moveOK(mv))
+				{
+					list.add((HexMove) mv.clone());
+				}
+			}
+		}
+		int which = Util.randInt(0, list.size() - 1);
+		return list.get(which);
+	}
+
+}
