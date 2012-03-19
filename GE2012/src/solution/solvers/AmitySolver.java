d75a2e19556ddceec5a21ee9d664d102f4e79aa7
sdellatorre <sdellatorre@mnil1>
2012-03-19 16:22:17 -0400

    [no commit message]

---
+++ GE2012/src/solution/solvers/AmitySolver.java
@@ -0,0 +1,14 @@
+package solution.solvers;
+import java.util.List;
+
+/**
+ * Class documentation.
+ *
+ * @author Mike DiBuduo
+ */
+public interface AmitySolver 
+{
+	public float getWeight();
+	public List<WeightedPoint> getPoints();
+	
+}
