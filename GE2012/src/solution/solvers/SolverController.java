bb8b96b54562116039878f996180f565ee6c0ff4
drdanielfc <drdanielfc@localhost>
2012-04-06 02:11:22 -0400

    got it to connect both sides

--- GE2012/src/solution/solvers/SolverController.java
+++ GE2012/src/solution/solvers/SolverController.java
@@ -9,6 +9,7 @@ import solution.board.HexPoint;
 import solution.board.Player;
 import solution.board.implementations.indivBoard.IndivBoard;
 import solution.board.implementations.indivBoard.IndivNode;
+import solution.debug.DebugWindow;
 
 /**
  * This controls all our solvers and generates the master weight table
@@ -46,26 +47,123 @@ public class SolverController
 		// follow chain down board
 		return followChain();
 
+		// TODO: if we have chains across, start to fill them in
+
 		// return new HexPoint(6, 'g');
 	}
 
 	private HexPoint followChain()
 	{
+		List<HexPoint> possible = new ArrayList<HexPoint>();
+
 		for (IndivNode node : indivBoard.getPoints())
 		{
 			if (node.getOccupied() == Player.ME)
 			{
-				List<HexPoint> chains = node.getTwoChains();
+				List<HexPoint> chains = node.getTwoChains();// indivBoard);
 				for (HexPoint pnt : chains)
 				{
-					if (indivBoard.getNode(pnt).getOccupied() == Player.EMPTY)
+					if (indivBoard.getNode(pnt).getOccupied() == Player.EMPTY && IndivNode.empty(pnt.connections(node.getPoints().get(0)), indivBoard))
 					{
-						return pnt;
+						possible.add(pnt);
 					}
 				}
 			}
 		}
-		return null;
+
+		Iterator<HexPoint> itr = possible.iterator();
+
+		int left = Integer.MAX_VALUE;
+		HexPoint bestLeft = null;
+
+		int right = Integer.MAX_VALUE;
+		HexPoint bestRight = null;
+
+		do
+		{
+			HexPoint h = itr.next();
+			
+			if (curr.getConnectRoute() == CurrentGame.CONNECT_LETTERS)
+			{
+				System.out.println("letters");
+			}
+			else
+				System.out.println("nums");
+
+			if (curr.getConnectRoute() == CurrentGame.CONNECT_LETTERS)
+			{
+				if (h.getY() < 'f')
+				{
+					int dist = calculateDistance(h);
+					if (dist < left)
+					{
+						bestLeft = h;
+						left = dist;
+					}
+				}
+				else
+				{
+					int dist = calculateDistance(h);
+					if (dist < right)
+					{
+						bestRight = h;
+						right = dist;
+					}
+				}
+			}
+			else
+			{
+				if (h.getX() < 5)
+				{
+					int dist = calculateDistance(h);
+					if (dist < left)
+					{
+						bestLeft = h;
+						left = dist;
+					}
+				}
+				else
+				{
+					int dist = calculateDistance(h);
+					if (dist < right)
+					{
+						bestRight = h;
+						right = dist;
+					}
+				}
+			}
+
+		} while (itr.hasNext());
+		
+		if (left > right && bestLeft != null)
+			return bestLeft;
+		else
+			return bestRight;
+
+	}
+	
+	
+
+	private int calculateDistance(HexPoint pnt)
+	{
+		if (curr.getConnectRoute() == CurrentGame.CONNECT_LETTERS)
+		{
+			
+			int i = pnt.getY() - 'a';
+			int j = 'k' - pnt.getY();
+			
+			DebugWindow.println(i+" "+j);
+
+			return Math.min(i, j);
+		}
+		else
+		{
+			int i = pnt.getX() - 1;
+			int j = 11 - pnt.getX();
+
+			return Math.min(i, j);
+		}
+
 	}
 
 	/**
@@ -78,7 +176,7 @@ public class SolverController
 		{
 			if (node.getOccupied() == Player.ME)
 			{
-				List<HexPoint> chains = node.getTwoChains();
+				List<HexPoint> chains = node.getTwoChains();// indivBoard);
 				for (HexPoint pnt : chains)
 				{
 					if (indivBoard.getNode(pnt).getOccupied() == Player.ME)
