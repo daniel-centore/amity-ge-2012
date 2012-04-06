ef341e1b41966be7bbc43bed46dfff1b51a09faa
drdanielfc <drdanielfc@localhost>
2012-04-06 00:00:10 -0400

    fixed some inconsistencies. began framework.

--- GE2012/src/solution/board/PointUtilities.java
+++ GE2012/src/solution/board/PointUtilities.java
@@ -13,25 +13,6 @@ import java.util.List;
  */
 public class PointUtilities
 {
-	/**
-	 * Converts the column from char notation to int
-	 * @param c The column
-	 */
-	public static int toInt(char c)
-	{
-		return ((int) c) - 97; // 97 is the unicode value of 'a'
-	}
-
-	/**
-	 * Converts a {@link HexPoint} into a regular {@link Point}.
-	 * Shouldn't need to be used much
-	 * @param point A {@link HexPoint}
-	 * @return A {@link Point}
-	 */
-	public static Point toPoint(HexPoint point)
-	{
-		return new Point(point.getX(), toInt(point.getY()));
-	}
 	
 	/**
 	 * Generates a {@link List} of {@link HexPoint}s which are directly surrounding a point
--- GE2012/src/solution/solvers/SolverController.java
+++ GE2012/src/solution/solvers/SolverController.java
@@ -1,14 +1,7 @@
 package solution.solvers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-
 import solution.CurrentGame;
 import solution.board.HexPoint;
-import solution.board.Player;
-import solution.solvers.multiplied.MultSolver;
 
 /**
  * This controls all our solvers and generates the master weight table
@@ -19,62 +12,21 @@ import solution.solvers.multiplied.MultSolver;
  */
 public class SolverController
 {
-	private List<AmitySolver> solvers; //list of solvers
-	private HashMap<HexPoint, WeightedPoint> map;	// map of the points on our grid
-	private CurrentGame curr; //Current game
-	
+	private CurrentGame curr; // Current game
+
 	public SolverController(CurrentGame curr)
 	{
 		this.curr = curr;
-		solvers = new ArrayList<AmitySolver>();
-		solvers.add(new MultSolver(curr));
 	}
-	
+
 	/**
-	 * Gets the highest rated move. PROBABLY VERY SLOW.
-	 * @return the "best move" possible according to our solver
+	 * Chooses our next move
+	 * @return
 	 */
-	public WeightedPoint getMove()
+	public HexPoint getMove()
 	{
-		// clear out the old map
-		map = new HashMap<HexPoint, WeightedPoint>();
-		
-		// iterate through our solvers and merge em
-		for (AmitySolver solver : solvers)
-		{
-			List<WeightedPoint> pts = solver.getPoints();
-			float w = solver.getWeight();
-			
-			// for each point in each solver
-			for (WeightedPoint wp : pts)
-			{
-				HexPoint key = new HexPoint(wp.getX(), wp.getY());
-				WeightedPoint val = map.get(key);
-				if (val == null)
-				{
-					val = new WeightedPoint(key.getX(), key.getY(), wp.getWeight() * w);		// if another solver hasn't handled it yet
-					map.put(key, val);
-				}
-				else
-					val.setWeight(val.getWeight() * wp.getWeight() * w);		// if we need to merge the weights
-			}
-		}
-		
-		// find the highest rated move
-		Iterator<WeightedPoint> itr = map.values().iterator();
-		WeightedPoint largest = null;
-		
-		while (itr.hasNext())
-		{
-			WeightedPoint p = itr.next();
-			if (largest == null || p.getWeight() > largest.getWeight())
-			{
-				if (curr.getBoardController().getIndivBoard().getNode(p.getX(), p.getY()).getOccupied() == Player.EMPTY)	// only if the space is unoccupied
-					largest = p;
-			}
-		}
-		
-		return largest;
+		return null;
 	}
+
 
 }
