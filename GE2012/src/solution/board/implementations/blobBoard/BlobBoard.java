b248d5e6b8c0b188ef43db74efa6210327d314c5
drdanielfc <drdanielfc@localhost>
2012-04-06 13:26:52 -0400

    cleanup and improvements

--- GE2012/src/solution/board/implementations/blobBoard/BlobBoard.java
+++ GE2012/src/solution/board/implementations/blobBoard/BlobBoard.java
@@ -17,8 +17,6 @@ import solution.board.PointUtilities;
  */
 public class BlobBoard implements BoardInterface
 {
-	// TODO: Make it so all nodes of the same color at the APPROPRIATE edge of the board get linked together
-	// we need to figure out which edge we're working for...
 	
 	// All the blobs of a color
 	private List<BlobNode> nodes = new ArrayList<BlobNode>();
